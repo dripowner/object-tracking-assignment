352a5cc406ee828b889de725f42ff4488a8bcbdd
Ilya Goldobin <yxtagold.iliy@gmail.com>
2024-04-06 02:12:16 +0300

    add kalman object tracker for sort

--- SORT.py
+++ SORT.py
@@ -3,12 +3,137 @@ from scipy.optimize import linear_sum_assignment
 import numpy as np
 from trackers import IoU
 
-def linerar_assignment(cost_matrix):
+def linear_assignment(cost_matrix):
     x, y = linear_sum_assignment(cost_matrix)
     return np.array(list(zip(x, y)))
 
+def iou_batch(bb_test, bb_gt):
+  """
+  From SORT: Computes IOU between two bboxes in the form [x1,y1,x2,y2]
+  """
+  bb_gt = np.expand_dims(bb_gt, 0)
+  bb_test = np.expand_dims(bb_test, 1)
+  
+  xx1 = np.maximum(bb_test[..., 0], bb_gt[..., 0])
+  yy1 = np.maximum(bb_test[..., 1], bb_gt[..., 1])
+  xx2 = np.minimum(bb_test[..., 2], bb_gt[..., 2])
+  yy2 = np.minimum(bb_test[..., 3], bb_gt[..., 3])
+  w = np.maximum(0., xx2 - xx1)
+  h = np.maximum(0., yy2 - yy1)
+  wh = w * h
+  o = wh / ((bb_test[..., 2] - bb_test[..., 0]) * (bb_test[..., 3] - bb_test[..., 1])                                      
+    + (bb_gt[..., 2] - bb_gt[..., 0]) * (bb_gt[..., 3] - bb_gt[..., 1]) - wh)                                              
+  return(o) 
+
+def convert_bbox_to_z(bbox):
+  """
+  Takes a bounding box in the form [x1,y1,x2,y2] and returns z in the form
+    [x,y,s,r] where x,y is the centre of the box and s is the scale/area and r is
+    the aspect ratio
+  """
+  w = bbox[2] - bbox[0]
+  h = bbox[3] - bbox[1]
+  x = bbox[0] + w/2.
+  y = bbox[1] + h/2.
+  s = w * h    #scale is just area
+  r = w / float(h)
+  return np.array([x, y, s, r]).reshape((4, 1))
+
+
+def convert_x_to_bbox(x,score=None):
+  """
+  Takes a bounding box in the centre form [x,y,s,r] and returns it in the form
+    [x1,y1,x2,y2] where x1,y1 is the top left and x2,y2 is the bottom right
+  """
+  w = np.sqrt(x[2] * x[3])
+  h = x[2] / w
+  if(score==None):
+    return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.]).reshape((1,4))
+  else:
+    return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.,score]).reshape((1,5))
+
 class KalmanBoxTracker():
-    def __init__(self) -> None:
+    count = 0 # id of track
+    def __init__(self, bbox) -> None:
+        # x - state, z - measurement
+        # x: [x, y, r, s, x', y', r'], z: [x, y, r, s]
         self.kf = KalmanFilter(dim_x=7, dim_z=4)
-        pass
+        # from x to z
+        self.kf.H = np.array([[1,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,1,0,0,0,0],[0,0,0,1,0,0,0]])
+        # from x(k) to x(k+1)
+        self.kf.F = np.array([[1,0,0,0,1,0,0],[0,1,0,0,0,1,0],[0,0,1,0,0,0,1],[0,0,0,1,0,0,0],[0,0,0,0,1,0,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,1]])
+        # covariance matrix of measurements noises
+        self.kf.R[2:,2:] *= 10.
+        # covariance matrix of state (uncertainty in state)
+        self.kf.P[4:,4:] *= 1000. #give high uncertainty to the unobservable initial velocities
+        self.kf.P *= 10.
+        # covariance matrix of model error (consider that model works good enough)
+        self.kf.Q[-1,-1] *= 0.01
+        self.kf.Q[4:,4:] *= 0.01
+
+        self.kf.x[:4] = convert_bbox_to_z(bbox)
+        self.time_since_update = 0
+        self.id = KalmanBoxTracker.count
+        KalmanBoxTracker.count += 1
+        self.history = []
+        self.hits = 0
+        self.hit_streak = 0
+        self.age = 0
+    
+    def update(self,bbox):
+        """
+        Updates the state vector with observed bbox.
+        """
+        self.time_since_update = 0
+        self.history = []
+        self.hits += 1
+        self.hit_streak += 1
+        # add new measurement z
+        self.kf.update(convert_bbox_to_z(bbox))
+    
+    def predict(self):
+        """
+        Advances the state vector and returns the predicted bounding box estimate.
+        """
+        if((self.kf.x[6]+self.kf.x[2])<=0):
+            self.kf.x[6] *= 0.0
+        # calculate new state x
+        self.kf.predict()
+        self.age += 1
+        if(self.time_since_update>0):
+            self.hit_streak = 0
+        self.time_since_update += 1
+        self.history.append(convert_x_to_bbox(self.kf.x))
+        return self.history[-1]
+    
+    def get_state(self):
+        """
+        Returns the current bounding box estimate.
+        """
+        return convert_x_to_bbox(self.kf.x)
+    
+    def compare_boxes(detections,trackers,iou_thresh=0.3):
+
+        iou_matrix = np.zeros(shape=(len(detections),len(trackers)),dtype=np.float32)
+
+        for d,det in enumerate(detections):
+            for t,trk in enumerate(trackers):
+                iou_matrix[d,t] = IoU(det,trk)
+        
+        # calculate maximum iou for each pair through hungarian algorithm
+        row_id, col_id = linear_sum_assignment(-iou_matrix)
+        matched_indices = np.transpose(np.array([row_id,col_id]))
+        # geting matched ious
+        iou_values = np.array([iou_matrix[row_id,col_id] for row_id,col_id in matched_indices])
+        best_indices = matched_indices[iou_values > iou_thresh]
+
+        unmatched_detection_indices = np.array([d for d in range(len(detections)) if d not in best_indices[:,0]])  
+        unmatched_trackers_indices = np.array([t for t in range(len(trackers)) if t not in best_indices[:,1]])
+
+        return best_indices,unmatched_detection_indices,unmatched_trackers_indices
+
+class SORT():
+   def __init__(self) -> None:
+      pass
+
 
