b218beff1fa9e37903f37c56950f4a656a1bc0bd
Ilya Goldobin <yxtagold.iliy@gmail.com>
2024-04-10 01:24:58 +0300

    add SORT

--- SORT.py
+++ SORT.py
@@ -1,56 +1,54 @@
 from filterpy.kalman import KalmanFilter
 from scipy.optimize import linear_sum_assignment
 import numpy as np
-from trackers import IoU
-
-def linear_assignment(cost_matrix):
-    x, y = linear_sum_assignment(cost_matrix)
-    return np.array(list(zip(x, y)))
-
-def iou_batch(bb_test, bb_gt):
-  """
-  From SORT: Computes IOU between two bboxes in the form [x1,y1,x2,y2]
-  """
-  bb_gt = np.expand_dims(bb_gt, 0)
-  bb_test = np.expand_dims(bb_test, 1)
-  
-  xx1 = np.maximum(bb_test[..., 0], bb_gt[..., 0])
-  yy1 = np.maximum(bb_test[..., 1], bb_gt[..., 1])
-  xx2 = np.minimum(bb_test[..., 2], bb_gt[..., 2])
-  yy2 = np.minimum(bb_test[..., 3], bb_gt[..., 3])
-  w = np.maximum(0., xx2 - xx1)
-  h = np.maximum(0., yy2 - yy1)
-  wh = w * h
-  o = wh / ((bb_test[..., 2] - bb_test[..., 0]) * (bb_test[..., 3] - bb_test[..., 1])                                      
-    + (bb_gt[..., 2] - bb_gt[..., 0]) * (bb_gt[..., 3] - bb_gt[..., 1]) - wh)                                              
-  return(o) 
+from utils import IoU
+from typing import List
 
 def convert_bbox_to_z(bbox):
-  """
-  Takes a bounding box in the form [x1,y1,x2,y2] and returns z in the form
+    """
+    Takes a bounding box in the form [x1,y1,x2,y2] and returns z in the form
     [x,y,s,r] where x,y is the centre of the box and s is the scale/area and r is
     the aspect ratio
-  """
-  w = bbox[2] - bbox[0]
-  h = bbox[3] - bbox[1]
-  x = bbox[0] + w/2.
-  y = bbox[1] + h/2.
-  s = w * h    #scale is just area
-  r = w / float(h)
-  return np.array([x, y, s, r]).reshape((4, 1))
-
+    """
+    w = bbox[2] - bbox[0]
+    h = bbox[3] - bbox[1]
+    x = bbox[0] + w/2.
+    y = bbox[1] + h/2.
+    s = w * h    #scale is just area
+    r = w / float(h)
+    return np.array([x, y, s, r]).reshape((4, 1))
 
 def convert_x_to_bbox(x,score=None):
-  """
-  Takes a bounding box in the centre form [x,y,s,r] and returns it in the form
+    """
+    Takes a bounding box in the centre form [x,y,s,r] and returns it in the form
     [x1,y1,x2,y2] where x1,y1 is the top left and x2,y2 is the bottom right
-  """
-  w = np.sqrt(x[2] * x[3])
-  h = x[2] / w
-  if(score==None):
-    return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.]).reshape((1,4))
-  else:
-    return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.,score]).reshape((1,5))
+    """
+    w = np.sqrt(x[2] * x[3])
+    h = x[2] / w
+    if(score==None):
+        return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.]).reshape((1,4))
+    else:
+        return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.,score]).reshape((1,5))
+  
+def compare_boxes(detections,trackers,iou_thresh=0.3):
+
+    iou_matrix = np.zeros(shape=(len(detections),len(trackers)),dtype=np.float32)
+
+    for d,det in enumerate(detections):
+        for t,trk in enumerate(trackers):
+            iou_matrix[d,t] = IoU(det,trk)
+    
+    # calculate maximum iou for each pair through hungarian algorithm
+    row_id, col_id = linear_sum_assignment(-iou_matrix)
+    matched_indices = np.transpose(np.array([row_id,col_id]))
+    # geting matched ious
+    iou_values = np.array([iou_matrix[row_id,col_id] for row_id,col_id in matched_indices])
+    best_indices = matched_indices[iou_values > iou_thresh]
+
+    unmatched_detection_indices = np.array([d for d in range(len(detections)) if d not in best_indices[:,0]])  
+    unmatched_trackers_indices = np.array([t for t in range(len(trackers)) if t not in best_indices[:,1]])
+
+    return best_indices,unmatched_detection_indices,unmatched_trackers_indices
 
 class KalmanBoxTracker():
     count = 0 # id of track
@@ -72,38 +70,30 @@ class KalmanBoxTracker():
         self.kf.Q[4:,4:] *= 0.01
 
         self.kf.x[:4] = convert_bbox_to_z(bbox)
-        self.time_since_update = 0
         self.id = KalmanBoxTracker.count
         KalmanBoxTracker.count += 1
         self.history = []
-        self.hits = 0
-        self.hit_streak = 0
-        self.age = 0
+        self.time_since_last_update = 0
     
     def update(self,bbox):
         """
-        Updates the state vector with observed bbox.
+        Add new measurement z from detection bbox
         """
-        self.time_since_update = 0
+        # add new measurement 
         self.history = []
-        self.hits += 1
-        self.hit_streak += 1
-        # add new measurement z
+        self.time_since_last_update = 0
         self.kf.update(convert_bbox_to_z(bbox))
     
     def predict(self):
         """
-        Advances the state vector and returns the predicted bounding box estimate.
+        Predict new state x and return predicted bbox
         """
+        # calculate new state x
         if((self.kf.x[6]+self.kf.x[2])<=0):
             self.kf.x[6] *= 0.0
-        # calculate new state x
         self.kf.predict()
-        self.age += 1
-        if(self.time_since_update>0):
-            self.hit_streak = 0
-        self.time_since_update += 1
         self.history.append(convert_x_to_bbox(self.kf.x))
+        self.time_since_last_update += 1
         return self.history[-1]
     
     def get_state(self):
@@ -111,29 +101,39 @@ class KalmanBoxTracker():
         Returns the current bounding box estimate.
         """
         return convert_x_to_bbox(self.kf.x)
-    
-    def compare_boxes(detections,trackers,iou_thresh=0.3):
-
-        iou_matrix = np.zeros(shape=(len(detections),len(trackers)),dtype=np.float32)
-
-        for d,det in enumerate(detections):
-            for t,trk in enumerate(trackers):
-                iou_matrix[d,t] = IoU(det,trk)
-        
-        # calculate maximum iou for each pair through hungarian algorithm
-        row_id, col_id = linear_sum_assignment(-iou_matrix)
-        matched_indices = np.transpose(np.array([row_id,col_id]))
-        # geting matched ious
-        iou_values = np.array([iou_matrix[row_id,col_id] for row_id,col_id in matched_indices])
-        best_indices = matched_indices[iou_values > iou_thresh]
-
-        unmatched_detection_indices = np.array([d for d in range(len(detections)) if d not in best_indices[:,0]])  
-        unmatched_trackers_indices = np.array([t for t in range(len(trackers)) if t not in best_indices[:,1]])
-
-        return best_indices,unmatched_detection_indices,unmatched_trackers_indices
 
 class SORT():
-   def __init__(self) -> None:
-      pass
+    def __init__(self, threshold=0.3, max_age=3) -> None:
+        self.trackers = []
+        self.iou_threshold = threshold
+        # amount of frames track live without matching
+        self.max_age = max_age
+    
+    def update(self, dets: List[List]) -> None:
+        # get tracks prediction bboxes
+        self.trackers = [tracker for tracker in self.trackers if not np.any(np.isnan(tracker.predict()))]
+
+        trks = [tracker.get_state()[0].tolist() for tracker in self.trackers]
+
+        # compare detection and tracks predictions based on IOU
+        matched, unmatched_dets, unmatched_tracks = compare_boxes(dets, trks, iou_thresh=self.iou_threshold)
+
+        out = {}
+        # Then we will update the kalman filter with measurements
+        # for each detection we maintain seperate filter
+        for detection_num, tracker_num in matched:
+            self.trackers[tracker_num].update(dets[detection_num])
+            # out[detection_num] = tracker_num
+            out[detection_num] = self.trackers[tracker_num].id
+        
+        # For all unmatched detections we will create new tracking in Kalman.
+        # it means new object comes to the frame
+        for i in unmatched_dets:
+            self.trackers.append(KalmanBoxTracker(dets[i]))
+        
+        # delete tracks without detection for max_age frames in a row
+        self.trackers = [tracker for tracker in self.trackers if tracker.time_since_last_update <= self.max_age]
+
+        return out
 
 
