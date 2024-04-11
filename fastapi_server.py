9a4e66c52506bf4ddf6dedd77323480994311224
Ilya Goldobin <yxtagold.iliy@gmail.com>
2024-04-11 22:37:08 +0300

    update readme, add metric computation, update strong_tracker

--- fastapi_server.py
+++ fastapi_server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, WebSocket
-from tracks.track_20_10_25 import track_data, country_balls_amount, track_name
+from tracks.track_10_10_25 import track_data, country_balls_amount, track_name
 import asyncio
 import glob
 import uvicorn
@@ -7,6 +7,7 @@ from SORT import SORT
 from deep_sort_realtime.deepsort_tracker import DeepSort
 from utils import bbox_to_deep_sort_format, get_frame, IoU
 from metrics import MOTA
+from strong_tracker import StrongTracker
 
 app = FastAPI(title='Tracker assignment')
 imgs = glob.glob('imgs/*')
@@ -18,10 +19,12 @@ strong_tracker = DeepSort(max_age=5,
                           n_init=1,
                           max_iou_distance=0.9,
                           bgr=False,
-                          max_cosine_distance=0.4,)
+                          max_cosine_distance=0.2,)
+strong05_tracker = StrongTracker()
 
 soft_results = {cb_id: [] for cb_id in range(country_balls_amount)}
 strong_results = {cb_id: [] for cb_id in range(country_balls_amount)}
+strong05_results = {cb_id: [] for cb_id in range(country_balls_amount)}
 print('Started')
 
 
@@ -73,6 +76,66 @@ def tracker_soft(el):
 
     el["data"] = data
     return el
+
+def tracker_strong_0_5(el):
+    """
+    Необходимо изменить у каждого словаря в списке значение поля 'track_id' так,
+    чтобы как можно более длительный период времени 'track_id' соответствовал
+    одному и тому же кантри болу.
+
+    Исходные данные: координаты рамки объектов
+
+    Ограничения:
+    - необходимо использовать как можно меньше ресурсов (представьте, что
+    вы используете embedded устройство, например Raspberri Pi 2/3).
+    -значение по ключу 'cb_id' является служебным, служит для подсчета метрик качества
+    вашего трекера, использовать его в алгоритме трекера запрещено
+    - запрещается присваивать один и тот же track_id разным объектам на одном фрейме
+    """
+    data = el["data"]
+    curr_frame_id = el["frame_id"]
+    curr_frame = get_frame(curr_frame_id, track_name)
+    if curr_frame is None:
+        for obj in data:
+            if len(obj["bounding_box"]) != 0:
+                obj["track_id"] = "?"
+            else:
+                obj["track_id"] = "not_detected"
+        el["data"] = data
+        return el
+    else:
+        bboxes = []
+        # get bboxes of detectet cbs
+        for obj in data:
+            if len(obj["bounding_box"]) != 0:
+                bboxes.append(obj["bounding_box"])
+            else:
+                strong05_results[obj["cb_id"]].append("not_detected")
+        # get track_ids for detected bboxes
+        track_ids = strong05_tracker.update(bboxes, curr_frame)
+        # assign track_id for every detected bbox
+        track_ids_counter = 0
+        used_track_ids = set()
+        for obj in data:
+            if len(obj["bounding_box"]) != 0:
+                track_id = track_ids.get(track_ids_counter, "?")
+
+                if track_id != "?":
+                    track_id = int(track_id)
+                    if track_id in used_track_ids:
+                        track_id = "?"
+                    else:
+                        used_track_ids.add(track_id)
+                obj["track_id"] = track_id
+                
+                strong05_results[obj["cb_id"]].append(track_id)
+
+                track_ids_counter += 1
+            else:
+                obj["track_id"] = "not_detected"
+
+        el["data"] = data
+        return el
 
 
 def tracker_strong(el):
@@ -158,19 +221,22 @@ async def websocket_endpoint(websocket: WebSocket):
     # класса CountryBall на фронте
     await websocket.send_text(str(country_balls))
     for el in track_data:
-        await asyncio.sleep(0.6)
+        await asyncio.sleep(0.3)
         # TODO: part 1
         el_soft = tracker_soft(el)
         # TODO: part 2
-        el = tracker_strong(el)
+        el_strong = tracker_strong(el)
+        el_strong_0_5 = tracker_strong_0_5(el)
         # отправка информации по фрейму
-        await websocket.send_json(el)
+        await websocket.send_json(el_strong_0_5)
     # most_common_track_ids = {cb_id: Counter(results).most_common(1)[0][0] for cb_id, results in soft_results.items()}
     # print(most_common_track_ids)
-    print(soft_results)
-    print(strong_results)
+    # print(soft_results)
+    # print(strong_results)
+    # print(strong05_results)
     print(f"soft mota: {MOTA(soft_results)}")
     print(f"strong mota: {MOTA(strong_results)}")
+    print(f"strong05 mota: {MOTA(strong05_results)}")
     print('Bye..')
 
 if __name__ == "__main__":
--- strong_tracker.py
+++ strong_tracker.py
@@ -15,7 +15,7 @@ def compare_boxes_with_embeds(detections, trackers, detections_embeds, trackers_
 
     for d,det in enumerate(detections):
         for t,trk in enumerate(trackers):
-            iou_matrix[d,t] = 1*IoU(det,trk) + 1*cosine_similarity([np.squeeze(detections_embeds[d])], [np.squeeze(trackers_embeds[t])])[0][0]
+            iou_matrix[d,t] = 0.8*IoU(det,trk) + 0.2*cosine_similarity([np.squeeze(detections_embeds[d])], [np.squeeze(trackers_embeds[t])])[0][0]
     
     # calculate maximum iou for each pair through hungarian algorithm
     row_id, col_id = linear_sum_assignment(-iou_matrix)
@@ -29,25 +29,25 @@ def compare_boxes_with_embeds(detections, trackers, detections_embeds, trackers_
 
     return best_indices,unmatched_detection_indices,unmatched_trackers_indices
 
-def transform_bbox(bbox):
-    x1, y1, x2, y2 = bbox
-    w = x2 - x1
-    h = y2 - y1
-    y1 += h
-    y2 += h
-    x1 += w/2
-    x2 += w/2
-    return [x1, y1, x2, y2]
+# def transform_bbox(bbox):
+#     x1, y1, x2, y2 = bbox
+#     w = x2 - x1
+#     h = y2 - y1
+#     y1 += h
+#     y2 += h
+#     x1 += w/2
+#     x2 += w/2
+#     return [x1, y1, x2, y2]
 
 def cut_preprocessed_bbox(frame, bbox, processing):
-    x1, y1, x2, y2 = transform_bbox(bbox)
     image_w, image_h = frame.shape[1], frame.shape[0]
+    x1, y1, x2, y2 = bbox
     cut = frame[max(int(y1), 0):min(int(y2), image_h), 
                 max(int(x1), 0):min(int(x2), image_w)]
     return processing(cut)[None, :, :, :]
 
 class StrongTracker(SORT):
-    def __init__(self, threshold=0.3, max_age=3) -> None:
+    def __init__(self, threshold=0.3, max_age=4) -> None:
         super().__init__(threshold, max_age)
         self.extractor = nn.Sequential(*list(mobilenet_v3_small(pretrained=True).children())[:-1])
         self.processing = transforms.Compose([
